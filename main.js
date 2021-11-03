35278f786dfa2e41f842d0177378cf8b03ab0d57
Vishal Agrawal <vishalagrawalva22@gmail.com>
2021-11-03 11:50:22 +0530

    Add pages

--- main.js
+++ main.js
@@ -1,6 +1,36 @@
 const defaultImageURL = "./images/default_book_logo.svg";
 
 let bookList = [];
+let currentlyPage = 0;
+let prevButton = document.querySelector("#prev");
+let nextButton = document.querySelector("#next");
+let curPage = document.querySelector("#cur-page");
+let lastPage = document.querySelector("#last-page");
+
+nextButton.onclick = function () {
+    if (3 * (currentlyPage + 1) < bookList.length) {
+        clearScreen();
+        currentlyPage++;
+        curPage.textContent = currentlyPage + 1;
+        displayBook();
+    } else {
+        alert("Already at last page!");
+    }
+}
+
+prevButton.onclick = function () {
+    if (3 * (currentlyPage - 1) >= 0) {
+        clearScreen();
+        currentlyPage--;
+        curPage.textContent = currentlyPage + 1;
+        displayBook();
+    } else {
+        alert("Already at first page!");
+    }
+}
+
+curPage.textContent = 1;
+lastPage.textContent = 1;
 
 function Book(title, author, pageCount, isRead, imageUrl) {
     this.title = title;
@@ -18,6 +48,23 @@ Book.prototype.getActionStatus = function () {
     return this.isRead ? "Mark as pending" : "Mark as finished";
 }
 
+function clearScreen() {
+    let l = 3 * currentlyPage;
+    let r = Math.min(3 * currentlyPage + 3, bookList.length);
+    for (let i = l; i < r; i++) {
+        book = document.querySelector(`[data-index="${i}"]`);
+        book.remove();
+    }
+}
+
+function displayBook() {
+    let l = 3 * currentlyPage;
+    let r = Math.min(3 * currentlyPage + 3, bookList.length);
+    for (let i = l; i < r; i++) {
+        addBookToDisplay(i);
+    }
+}
+
 function exitForm() {
     let inputForm = document.querySelector("#form-container");
     inputForm.classList.add("hidden");
@@ -33,8 +80,14 @@ form.addEventListener('submit', event => {
     let author = data["book-author"].value;
     let pageCount = data["book-page-count"].value;
     let imageURL = data["book-image-url"].value;
+    clearScreen();
     bookList.push(new Book(title, author, pageCount, false, imageURL));
-    addBookToLibrary(bookList.length - 1);
+    let curLen = bookList.length;
+    let maxLen = Math.ceil(curLen / 3); 
+    lastPage.textContent = maxLen;
+    curPage.textContent = maxLen;
+    currentlyPage = maxLen - 1;
+    displayBook();
     exitForm();
 });
 
@@ -56,21 +109,24 @@ function changeStatus(index) {
 
 function deleteBook(index) {
     let len = bookList.length;
-    let book = document.querySelector(`[data-index="${index}"]`);
-    book.remove();
+    clearScreen();
     for (let i = index + 1; i < len; i++) {
-        book = document.querySelector(`[data-index="${i}"]`);
-        book.remove();
         bookList[i - 1] = bookList[i];
     }
     bookList.pop();
-
-    for (let i = index; i < len - 1; i++) {
-        addBookToLibrary(i);
+    let curLen = bookList.length;
+    let maxLen = Math.ceil(curLen / 3);
+    if (maxLen != 0) {
+        lastPage.textContent = maxLen;
     }
+    if (curLen % 3 == 0 && currentlyPage != 0) {
+        currentlyPage--;
+        curPage.textContent = currentlyPage + 1;
+    }
+    displayBook();
 }
 
-function addBookToLibrary(index) {
+function addBookToDisplay(index) {
     let bookItemHtml = `<article class="book-item" data-index=${index}>
                         <section class="book-image">
                         <img src="${bookList[index].imageUrl}" onerror=this.src="${defaultImageURL}" />
@@ -109,11 +165,9 @@ function addBookToLibrary(index) {
     bookListSection.innerHTML += bookItemHtml;
 }
 
-let bookArray = [["Harry Potter and the Chamber of Secrets", "J. K. Rowling", 251, true, "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQonVotG4wBf9-cvKmdGf9UIM9ITHMfexp-ZtD_9xAkx9m1fKtR"], ["Lorem ipsum dolor sit amet", "Lorem ipsum dolor sit amet", 243, false, null], ["Amazing book title", "Famous author", 263, false, null]];
+let bookArray = [["Harry Potter and the Chamber of Secrets", "J. K. Rowling", 251, true, "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQonVotG4wBf9-cvKmdGf9UIM9ITHMfexp-ZtD_9xAkx9m1fKtR"], ["Lorem ipsum dolor sit amet, consectetur adipiscing", "Lorem ipsum dolor sit amet, consectetur adipiscing", 243, false, null], ["Amazing book title", "Famous author", 263, false, null]];
 for (const book of bookArray) {
     bookList.push(new Book(...book));
 }
 
-for (let i = 0; i < bookList.length; i++) {
-    addBookToLibrary(i);
-}
+displayBook();
